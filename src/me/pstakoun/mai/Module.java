f79ba7cdf530b5e944976dd176d5ca4181d40262
Peter Stakoun <peter@stakoun.com>
2015-01-03 23:28:47 -0500

    description

--- src/me/pstakoun/mai/Module.java
+++ src/me/pstakoun/mai/Module.java
@@ -3,6 +3,11 @@ package me.pstakoun.mai;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * All modules must implement this interface in order to be recognized.
+ * This provides them with methods to communicate with MAI, the user, and other modules.
+ * @author Peter Stakoun
+ */
 public interface Module
 {
 	/**
